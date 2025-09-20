7f2870274609b3d2e4b56f3b1d93198a83a058ab
Adam Thieme <adam@cs.ucla.edu>
2025-09-20 05:29:11 +0000

    debugging

--- repo/repo.go
+++ repo/repo.go
@@ -14,9 +14,9 @@ import (
 	"github.com/named-data/ndnd/std/ndn"
 	"github.com/named-data/ndnd/std/object"
 	local_storage "github.com/named-data/ndnd/std/object/storage"
-	//sec "github.com/named-data/ndnd/std/security"
-	//"github.com/named-data/ndnd/std/security/keychain"
-	//"github.com/named-data/ndnd/std/security/trust_schema"
+	sec "github.com/named-data/ndnd/std/security"
+	"github.com/named-data/ndnd/std/security/keychain"
+	"github.com/named-data/ndnd/std/security/trust_schema"
 )
 
 type Repo struct {
@@ -63,29 +63,6 @@ func (r *Repo) Start() (err error) {
 	if err = r.engine.Start(); err != nil {
 		return err
 	}
-
-	log.Debug(r, "new keychain")
-	//kc, err := keychain.NewKeyChain(r.nodeConfig.KeyChainUri, r.store)
-	//if err != nil {
-	//	return err
-	//}
-
-	log.Debug(r, "here")
-	// TODO: specify a real trust schema
-	//schema := trust_schema.NewNullSchema()
-
-	// testbed anchor, necessary for validating commands
-	anchors := r.nodeConfig.TrustAnchorNames()
-
-	// Create trust config
-	//trust, err := sec.NewTrustConfig(kc, schema, anchors)
-	//if err != nil {
-	//	return err
-	//}
-
-	log.Debug(r, "here")
-	// Attach data name as forwarding hint to cert Interests
-	//trust.UseDataNameFwHint = true
 
 	// Start NDN Object API client
 	// TODO: enable trust
