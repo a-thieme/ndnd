f9df8543cfb334303119fe5d8053a8b046d56cc4
Adam Thieme <adam@cs.ucla.edu>
2025-09-28 04:49:17 +0000

    checkpoint: fixed producer-facing

--- repo/producer-facing/producer_facing.go
+++ repo/producer-facing/producer_facing.go
@@ -6,9 +6,6 @@ import (
 	enc "github.com/named-data/ndnd/std/encoding"
 	"github.com/named-data/ndnd/std/log"
 	"github.com/named-data/ndnd/std/ndn"
-	sec_pib "github.com/named-data/ndnd/std/security/pib"
-	"github.com/named-data/ndnd/std/types/optional"
-	"time"
 )
 
 // RepoProducerFacing is the producer facing component of the repo
@@ -22,8 +19,6 @@ type RepoProducerFacing struct {
 
 	newCommandCallback func(*tlv.RepoCommand)
 }
-
-var pib *sec_pib.SqlitePib
 
 func (p *RepoProducerFacing) String() string {
 	return "producer-facing"
@@ -54,7 +49,7 @@ func (p *RepoProducerFacing) Start() error {
 	}
 
 	// Register command handler prefixes
-	p.repo.Engine.AttachHandler(p.externalNotifyPrefixN, p.onExternalNotify)
+	p.repo.Client.AttachCommandHandler(p.externalNotifyPrefixN, p.onCommand)
 	return nil
 }
 
@@ -71,51 +66,21 @@ func (p *RepoProducerFacing) Stop() error {
 }
 
 // onExternalNotify is called when a repo notify interest is received
-func (p *RepoProducerFacing) onExternalNotify(args ndn.InterestHandlerArgs) {
-	interest := args.Interest
-	log.Info(p, "got new command, interest name", interest.Name())
+func (p *RepoProducerFacing) onCommand(name enc.Name, content enc.Wire, reply func(wire enc.Wire) error) {
+	log.Info(p, "got new command, interest name", name)
 
-	if interest.AppParam() == nil {
-		log.Trace(p, "Notify interest has no app param, ignoring")
-		return
-	}
-
-	// TODO: do trust schema validation here
-	command, err := tlv.ParseRepoCommand(enc.NewWireView(interest.AppParam()), true)
+	command, err := tlv.ParseRepoCommand(enc.NewWireView(content), false)
 	if err != nil {
 		log.Trace(p, "Failed to parse notify app param", "err", err)
 		return
 	}
 
-	log.Info(p, "Received external notify command", command)
-
-	// Reply to the command - "Repo has received the command"
+	log.Debug(p, "making response")
 	sr := &tlv.RepoStatusResponse{
 		Target: command.Target,
 		Status: "recieved",
 	}
 
-	log.Debug(p, "making data")
-	data, err := p.repo.Engine.Spec().MakeData(
-		interest.Name().WithVersion(enc.VersionUnixMicro),
-		&ndn.DataConfig{
-			ContentType: optional.Some(ndn.ContentTypeBlob), // TODO: see if this needs to be set
-			Freshness:   optional.Some(10 * time.Second),
-		},
-		sr.Encode(),
-		nil, // TODO: sign this data
-	)
-	if err != nil {
-		log.Error(p, "Failed to make reply data", "err", err)
-		return
-	}
-	err = args.Reply(data.Wire)
-	if err != nil {
-		log.Debug(p, "error replying:", err)
-	}
-
-	log.Debug(p, "replied with data", sr)
-	log.Debug(p, "replied with data", data)
-	// p.newCommandCallback(command)
-	log.Debug(p, "after callback for command", command)
+	log.Debug(p, "responding")
+	reply(sr.Encode())
 }
